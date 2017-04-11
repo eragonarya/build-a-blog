e87625a5523e18f143b39da73e8d18a9e6e26eb8
Ninja <krutilcody@yahoo.com>
2017-04-10 21:54:37 -0500

    Finished first half of 'education.launchcode.org/web-fundamentals/assignments/build-a-blog/'

--- main.py
+++ main.py
@@ -14,12 +14,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a,**kw)
+
+    def render_str(self,template,**params):
+        t=jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self,template, **kw):
+        self.write(self.render_str(template,**kw))
+
+class Blog(db.Model):
+        title = db.StringProperty(required = True)
+        blog = db.TextProperty(required = True)
+        created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self,title='',blog='',error=''):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC limit 5")
+        self.render("blog.html",title=title,blog=blog,error=error, blogs=blogs)
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+class Submit(Handler):
+    def render_front(self,title='',blog='',error=''):
+        self.render("submission.html",title=title,blog=blog,error=error)
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get('title')
+        blog = self.request.get('blog')
+        if title and blog:
+            b = Blog(title = title, blog = blog)
+            b.put()
+
+            self.redirect("/")
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front(title,blog,error)
+
+app = webapp2.WSGIApplication([('/',MainPage),('/newpost',Submit)], debug = True)
